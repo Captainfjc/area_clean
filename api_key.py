3a3e7495c06b80f84c63d59d40c8d6ddc7dd3799
fanjc2 <fanjc2@lenovo.com>
2025-02-19 17:01:23 +0800

    apikey设置常量

---
+++ api_key.py
@@ -0,0 +1,8 @@
+# api_key.py
+DASHSCOPE_API_KEY = "sk-57e1cf104b2c485688468c64bfe945b9"  #通义api key
+#sk-e8d51fbf711647fea02d072a035eadcc
+ARK_API_KEY = 'd9381543-35e9-463f-b442-8f2630f2ed93' #豆包api key
+DEEPSEEK_API_KEY = 'sk-0e2c2121e0324f238c2405e481c8d759' #deepseek api key
+
+SILICONFLOW_API_Key = 'Bearer sk-njxawnbszsgihcdukfehdtqldlkiserrilifupmsapakhqak'
+NVIDIA_API_KEY = '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'
